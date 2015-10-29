91bcfd5f778e0a67aff611ff1879bd464a3efca1
Magnus <magnus@nissel.org>
2015-10-29 21:48:32 +0100

    Initial (empty) commit.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='openconc',
+      version='0.1',
+      description='An tool for corpus linguists',
+      url='https://github.com/magnusnissel/openconc',
+      author='Magnus Nissel',
+      author_email='magnus@nissel.org',
+      license='MIT',
+      packages=['openconc'],
+      zip_safe=False)
